f08f8fe8c3fb7a5355087081ffc80f518827eaca
jean <carlosvp0324@gmail.com>
2023-01-07 01:41:32 -0500

    initial commit

---
+++ source/calendar.py
@@ -0,0 +1,24 @@
+class Calendar:
+    def __init__(self, events) -> None:
+        self.events = events
+
+    def ist_events(self):
+        pass
+
+    def create_event(self):
+        pass
+
+    def show_event(self):
+        pass
+
+    def update_event(self):
+        pass
+
+    def delete_event(self):
+        pass
+
+    def show_next(self):
+        pass
+
+    def show_prev(self):
+        pass
